64636e3add832c7c2cd6c7bcfc9c70c1f79af9f4
franBautistaSomoDevOps <144240758+franBautistaSomoDevOps@users.noreply.github.com>
2023-10-30 09:23:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function saludo() {
+    alert("Hola Pepinillo Feliz")
+}
+
+
+
+/*var resultado = window.confirm('Estas seguro?');
+if (resultado === true) {
+    window.alert('Okay, si estas seguro.');
+} else { 
+    window.alert('Pareces indeciso');
+}*/
